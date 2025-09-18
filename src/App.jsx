79a8cceaea6be984598a3f25f9283fead8a1033e
cadenedam <cedam@purdue.edu>
2025-09-17 20:05:58 -0400

    Edited images source

--- src/App.jsx
+++ src/App.jsx
@@ -98,7 +98,7 @@ function App() {
                 </li>
                 <li>
                   Create a new repository and give it a name (e.g., "my-website"). Make sure the repository is set to public.
-                  <img src="/images/new_repo.PNG" alt="Repository creation screenshot" />
+                  <img src="/instructions-site/images/new_repo.PNG" alt="Repository creation screenshot" />
                 </li>
                 <li>
                   Download VSCode from {" "}
@@ -115,7 +115,7 @@ function App() {
                 </li>
                 <li>
                   Inside this folder, create a file named <code>index.html</code>.
-                  <img src="/images/index_create.PNG" />
+                  <img src="/instructions-site/images/index_create.PNG" />
                 </li>
                 <li>
                   In your new file, copy and paste the following basic HTML template:
@@ -147,7 +147,7 @@ git add .
 git commit -m "Initial commit"
 git push -u origin main`}
                   </pre>
-                  <img src="/images/terminal.PNG" />
+                  <img src="/instructions-site/images/terminal.PNG" />
                 </li>
                 <li>
                   Enable GitHub pages in your repository settings:
@@ -156,7 +156,7 @@ git push -u origin main`}
                     <li>Click on "Settings".</li>
                     <li>Scroll down to the "Pages" section.</li>
                     <li>Under "Source", select the branch "main" and click "Save".
-                      <img src="/images/publish_page.PNG" />
+                      <img src="/instructions-site/images/publish_page.PNG" />
                     </li>
                   </ul>
                 </li>
@@ -395,7 +395,7 @@ git push -u origin main`}
                     <li>Click on "Settings".</li>
                     <li>Scroll down to the "Pages" section.</li>
                     <li>Under "Source", select the branch "main" and click "Save".
-                      <img src="/images/publish_page.PNG" />
+                      <img src="/instructions-site/images/publish_page.PNG" />
                     </li>
                   </ul>
                 </li>
